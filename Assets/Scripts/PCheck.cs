cabe14f2f85850f2527537a9e56073c1c623f8f8
TurtleKwitty <5491534+TurtleKwitty@users.noreply.github.com>
2019-10-13 12:19:46 -0400

    HUD reacts to number of players

---
+++ Assets/Scripts/PCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCheck : MonoBehaviour
+{
+    private void Start()
+    {
+        if(PlayersNumber.NumberOfPlayers < 1)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
