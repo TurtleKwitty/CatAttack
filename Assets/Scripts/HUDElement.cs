55bab450e7188de86e66461650a1eb2266128e01
TurtleKwitty <5491534+TurtleKwitty@users.noreply.github.com>
2019-10-13 12:11:42 -0400

    Hud Updates

---
+++ Assets/Scripts/HUDElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDElement : MonoBehaviour
+{
+    public int PlayerID;
+    public int ResourceID;
+
+    void UpdateHUDElement(int amount)
+    {
+        Debug.Log(ResourceID);
+        GetComponent<TMPro.TextMeshProUGUI>().text = amount.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HUDManager.Delegates[PlayerID, ResourceID] = UpdateHUDElement;
+        Debug.Log(PlayerID);
+        Debug.Log(ResourceID);
+    }
+}
--- Assets/Scripts/PlayerResources.cs
+++ Assets/Scripts/PlayerResources.cs
@@ -8,18 +8,30 @@ public class PlayerResources : MonoBehaviour
     public int iron = 0;
     public int stone = 0;
 
+    int PlayerID = -1;
+
+    private void Start()
+    {
+        if (gameObject.name == "Player1(Clone)") PlayerID = 0;
+        if (gameObject.name == "Player2(Clone)") PlayerID = 1;
+        Debug.Log(PlayerID);
+    }
+
     public void AddResource(int amount, string id)
     {
         switch(id)
         {
             case "tree":
                 wood += amount;
-                break;
-            case "ore":
-                iron += amount;
+                if (PlayerID >= 0) HUDManager.Delegates[PlayerID, 0](wood);
                 break;
             case "rock":
                 stone += amount;
+                if (PlayerID >= 0) HUDManager.Delegates[PlayerID, 1](stone);
+                break;
+            case "ore":
+                iron += amount;
+                if (PlayerID >= 0) HUDManager.Delegates[PlayerID, 2](iron);
                 break;
             default:
                 break;
